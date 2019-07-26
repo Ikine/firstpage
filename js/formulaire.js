39b74e3c32f336c89b37c8d37e7d45dc6a01a65b
Irvine <45358249+Ikine@users.noreply.github.com>
2019-07-26 16:41:43 +0200

    Delete formulaire.js

--- js/formulaire.js
+++
@@ -1,8 +0,0 @@
-var validation = document.getElementById("envoyer");
-var pseudo = document.getElementById("pseudo");
-var pseudo_m = document.getElementById("pseudo_manquant");
-validation.addEventListener("click" , f_valid);
-
-fonction f_valid(e){
-    if (prenom)
-}
